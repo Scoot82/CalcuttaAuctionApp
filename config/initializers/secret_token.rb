cd43663fc0f6cbd0693e2da623b250cdd1406327
Scott Fey <scottfey@gmail.com>
2012-07-18 09:24:42 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CalcuttaAuctionApp::Application.config.secret_token = 'fe4a12686c49529f4b61c164bc6f88a917db163777366ffea3a7608f610778eda28ff6ddb588b694380cd2120b53e64530e8d9c7f6a42796cf030c1adbf920fb'
