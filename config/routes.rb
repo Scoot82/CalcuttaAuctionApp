5753537cd2144951da509baec716836d9df9dd69
Scott Fey <scottfey@gmail.com>
2012-07-18 11:28:58 -0500

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 CalcuttaAuctionApp::Application.routes.draw do
+  root to: 'static_pages#home'
   
-  get "static_pages/home", controller: "static_pages", action: "home"
-  get "static_pages/help", controller: "static_pages", action: "help"
-  get "static_pages/about", controller: "static_pages", action: "about"
-  get "static_pages/contact", controller: "static_pages", action: "contact"
+  get "static_pages/help", controller: "static_pages", action: "help", :as => :help
+  get "static_pages/about", controller: "static_pages", action: "about", :as => :about
+  get "static_pages/contact", controller: "static_pages", action: "contact", :as => :contact
 
+  get "users/new", controller: "users", action: "new", :as => :signup
+  
 end
